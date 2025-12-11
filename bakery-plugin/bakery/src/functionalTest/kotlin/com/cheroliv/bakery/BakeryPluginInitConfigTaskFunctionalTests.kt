021cf11eec6c6cd4efaf620b1cc8f929f970eece
cheroliv <cheroliv@laposte.net>
2025-12-11 01:18:25 +0100

    ✨ (test)(bakery-plugin/bakery): Add functional tests for InitConfigTask

--- bakery-plugin/bakery/src/functionalTest/kotlin/com/cheroliv/bakery/BakeryPluginInitConfigTaskFunctionalTests.kt
+++ bakery-plugin/bakery/src/functionalTest/kotlin/com/cheroliv/bakery/BakeryPluginInitConfigTaskFunctionalTests.kt
@@ -1,4 +1,33 @@
 package com.cheroliv.bakery
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.io.TempDir
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory.getLogger
+import java.io.File
+import kotlin.test.BeforeTest
+
 class BakeryPluginInitConfigTaskFunctionalTests {
+    companion object {
+        private val log: Logger by lazy { getLogger(BakeryPluginInitSiteTaskFunctionalTests::class.java) }
+
+        fun info(message: String) {
+            message
+                .apply(log::info)
+                .run(::println)
+        }
+    }
+
+    @field:TempDir
+    lateinit var projectDir: File
+
+    @BeforeTest
+    fun prepare() {
+        // Do site.yml exist in root project folder?
+        assertThat(projectDir.resolve("site.yml"))
+            .describedAs("site.yml should not exists yet")
+            .doesNotExist()
+    }
+
+
 }
