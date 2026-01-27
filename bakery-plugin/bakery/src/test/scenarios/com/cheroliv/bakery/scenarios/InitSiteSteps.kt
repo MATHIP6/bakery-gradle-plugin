6b94dbba4cf7a43f05f8f8b20547d95d1f03fe10
cheroliv <cheroliv@laposte.net>
2026-01-27 07:11:33 +0100

    ✨ feat(test): Add step to verify buildScript content

--- bakery-plugin/bakery/src/test/scenarios/com/cheroliv/bakery/scenarios/InitSiteSteps.kt
+++ bakery-plugin/bakery/src/test/scenarios/com/cheroliv/bakery/scenarios/InitSiteSteps.kt
@@ -1,16 +1,22 @@
 package com.cheroliv.bakery.scenarios
 
-//import io.cucumber.java.en.*
-//import kotlinx.coroutines.runBlocking
-//import org.assertj.core.api.Assertions.assertThat
+import io.cucumber.java.en.And
+import org.assertj.core.api.Assertions.assertThat
 
 class InitSiteSteps(private val world: TestWorld) {
 
-//    @Given("a new Gradle project")
-//    fun unNouveauProjetGradle() {
-//        world.createGradleProject()
-//        assertThat(world.projectDir).exists()
-//    }
+    @And("I add a buildScript file with {string} as the config path in the dsl")
+    fun checkBuildScript(configfile: String) {
+        assertThat(
+            world.projectDir!!
+                .resolve("build.gradle.kts")
+                .readText()
+        ).describedAs("Should contains plugins block and bakery dsl.")
+            .contains(
+                "plugins { id(\"com.cheroliv.bakery\") }",
+                "bakery { configPath = file(\"site.yml\").absolutePath }"
+            )
+    }
 //
 //    @When("I am executing the task {string}")
 //    fun jExecuteLaTache(taskName: String) = runBlocking {
